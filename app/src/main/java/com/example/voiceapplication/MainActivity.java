ff85c54a7f6dfa0591645d3b3060843af9f86b9c
Denis Muthomi <muthomidenis62@gmail.com>
2019-12-02 23:57:45 +0300

    new class

--- app/src/main/java/com/example/voiceapplication/MainActivity.java
+++ app/src/main/java/com/example/voiceapplication/MainActivity.java
@@ -10,6 +10,7 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
